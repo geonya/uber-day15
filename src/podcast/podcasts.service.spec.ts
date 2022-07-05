028334ddb684267e59d9d897f46dd363eee46af3
geonya <geony8410@gmail.com>
2022-07-05 22:21:20 +0900

    podcast service unit testing setup

--- src/podcast/podcasts.service.spec.ts
+++ src/podcast/podcasts.service.spec.ts
@@ -1,3 +1,50 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Episode } from 'src/podcast/entities/episode.entity';
+import { Podcast } from 'src/podcast/entities/podcast.entity';
+import { PodcastsService } from 'src/podcast/podcasts.service';
+import { Repository } from 'typeorm';
+
+const mockRepository = () => ({
+  find: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+  findOne: jest.fn(),
+  delete: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('PodcastService', () => {
-  it.todo('Test it all!!');
+  let podcastService: PodcastsService;
+  let podcastRepository: MockRepository<Podcast>;
+  let episodeRepository: MockRepository<Episode>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        PodcastsService,
+        { provide: getRepositoryToken(Podcast), useValue: mockRepository() },
+        { provide: getRepositoryToken(Episode), useValue: mockRepository() },
+      ],
+    }).compile();
+    podcastService = module.get<PodcastsService>(PodcastsService);
+    podcastRepository = module.get(getRepositoryToken(Podcast));
+    episodeRepository = module.get(getRepositoryToken(Episode));
+  });
+
+  it('sould be defined', () => {
+    expect(podcastService).toBeDefined();
+  });
+
+  it.todo('getAllPodcasts');
+  it.todo('createPodcast');
+  it.todo('getPodcast');
+  it.todo('deletePodcast');
+  it.todo('updatePodcast');
+  it.todo('getEpisodes');
+  it.todo('getEpisode');
+  it.todo('createEpisode');
+  it.todo('deleteEpisode');
+  it.todo('updateEpisode');
 });
